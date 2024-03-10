cb6aa9711bb1ae25b8b92135cdd5c4c79e4a4b1b
Uday932 <uday.bhavsar.sio@gmail.com>
2024-03-10 17:15:42 +0100

    update css animation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Attendre que le DOM soit chargé avant d'exécuter le script
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialiser Typed.js avec l'ID de l'élément HTML cible
+    let typingText = new Typed("#text", {
+        strings : ["UDAY BHAVSAR"],
+        typeSpeed: 35
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init();
+});
